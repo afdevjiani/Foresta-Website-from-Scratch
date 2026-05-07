54d2f30eff646d43fa33b783323b1e015f496d69
hani <alidev487@gmail.com>
2026-05-07 04:43:31 +0400

    deploy: foresta.ae authDomain, auth proxy, fixed image extensions

--- video-control.js
+++ video-control.js
@@ -1,10 +1,1 @@
-// Heritage video slow-motion effect
-document.addEventListener('DOMContentLoaded', function() {
-    var heritageVideo = document.querySelector('.heritage-image video');
-    if (heritageVideo) {
-        heritageVideo.playbackRate = 0.7;
-        heritageVideo.addEventListener('loadedmetadata', function() {
-            heritageVideo.play();
-        });
-    }
-});
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".heritage-image video");e&&(e.playbackRate=.7,e.addEventListener("loadedmetadata",function(){e.play()}))});
